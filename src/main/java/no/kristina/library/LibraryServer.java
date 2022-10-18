4bf8811047a04627d8b2c62f1c00f1aaafc71581
mohammed <mohyus15@hotmail.com>
2022-10-19 00:54:41 +0200

    libray test does not run after throws all exception

--- src/main/java/no/kristina/library/LibraryServer.java
+++ src/main/java/no/kristina/library/LibraryServer.java
@@ -1,7 +1,21 @@
 package no.kristina.library;
+import org.eclipse.jetty.server.Server;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class LibraryServer {
+    private final Server server;
+    public LibraryServer(int port) {
+        this.server = new Server(0);
+
+    }
+
     public static void main(String[] args) {
         System.out.println("hello world");
+    }
+
+    public URL getURL() throws MalformedURLException {
+        return server.getURI().toURL();
     }
 }
