d62081ba9345467fbdbbda6cd43aa5e105429c49
mohammed <mohyus15@hotmail.com>
2022-10-26 19:44:52 +0200

    the reasen we are getting 404 there is no api box

--- src/test/java/no/kristina/library/LibraryServerTest.java
+++ src/test/java/no/kristina/library/LibraryServerTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.PATH;
 
 class LibraryServerTest {
 
@@ -21,18 +23,18 @@ class LibraryServerTest {
 
     @Test
     void shoudShowFrontPage() throws IOException {
-        var connection = openConnection();
+        var connection = openConnection("/");
         assertThat(connection.getResponseCode()).isEqualTo(200);
         assertThat(connection.getInputStream()).asString(StandardCharsets.UTF_8).contains("<title>kristina library</title>");
     }
     @Test
     void shoudShowdynamicContent() throws IOException {
-        var connection = openConnection('/api/books');
-        assertThat(connection.getResponseCode()).isEqualTo(200);
+        var connection = openConnection("/api/books");
+         assertThat(connection.getResponseCode()).isEqualTo(200);
         assertThat(connection.getInputStream()).asString(StandardCharsets.UTF_8).contains("[{\"title\":\"Hello World\"");
     }
 
-    private HttpURLConnection openConnection() throws IOException {
-        return (HttpURLConnection) server.getURL().openConnection();
+    private HttpURLConnection openConnection(String path) throws IOException {
+        return (HttpURLConnection) new URL( server.getURL(), path).openConnection();
     }
 }
