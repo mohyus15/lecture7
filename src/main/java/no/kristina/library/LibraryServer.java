8de6f0d1e73dada2f4a19b9ccb155607c12a8d01
mohammed <mohyus15@hotmail.com>
2022-10-26 17:34:09 +0200

    create webapp for jetty

--- src/main/java/no/kristina/library/LibraryServer.java
+++ src/main/java/no/kristina/library/LibraryServer.java
@@ -1,5 +1,9 @@
 package no.kristina.library;
+
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -8,6 +12,9 @@ public class LibraryServer {
     private final Server server;
     public LibraryServer(int port) {
         this.server = new Server(0);
+        server.setHandler(new HandlerList(
+                new WebAppContext(Resource.newClassPathResource("/webapp"),"/")
+        ));
 
     }
 
